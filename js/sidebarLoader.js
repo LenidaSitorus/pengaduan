193f9400b7ce005f375fab5d5a05e042dc1b4c8f
LenidaSitorus <lenidanathaniaivanasitorus@gmail.com>
2025-02-24 20:07:24 +0700

    Menghapus sidebarLoader.js

--- js/sidebarLoader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("components/sidebar.html") // Panggil sidebar dari folder components/
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("sidebar-container").innerHTML = data;
-    })
-    .catch((error) => console.error("Gagal memuat sidebar:", error));
-});
